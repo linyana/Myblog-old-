32ad5ab0c8811bd2cccf5d4f3f1517a18940fa30
unknown <1439713475@qq.com>
2022-03-13 23:19:49 +0800

    updata

--- src/JS/index.js
+++ src/JS/index.js
@@ -8,16 +8,16 @@ const main_motto3 = document.querySelector('.main .main_motto3');
 // width
 const screenWidth = window.screen.width;
 main.style.width = screenWidth + 'px';
-main_name.style.right = screenWidth * 0.120 + 'px';
-main_motto1.style.right = screenWidth * 0.180 + 'px';
-main_motto2.style.right = screenWidth * 0.150 + 'px';
-main_motto3.style.right = screenWidth * 0.150 + 'px';
+main_name.style.right = screenWidth * 0.12 + 'px';
+main_motto1.style.right = screenWidth * 0.18 + 'px';
+main_motto2.style.right = screenWidth * 0.15 + 'px';
+main_motto3.style.right = screenWidth * 0.15 + 'px';
 
 // height
 const screenHeight = window.screen.height - 120;
 main.style.height = screenHeight + 'px';
-main_name.style.top = screenHeight * 0.200 + 'px';
-main_motto1.style.top = screenHeight * 0.350 + 'px';
-main_motto2.style.top = screenHeight * 0.400 + 'px';
-main_motto3.style.top = screenHeight * 0.550 + 'px';
+main_name.style.top = screenHeight * 0.20 + 'px';
+main_motto1.style.top = screenHeight * 0.35 + 'px';
+main_motto2.style.top = screenHeight * 0.40 + 'px';
+main_motto3.style.top = screenHeight * 0.55 + 'px';
 
